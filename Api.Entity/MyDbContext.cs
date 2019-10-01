743de254184579253e4cb355003f8dc31b2a47d1
Vinicius de Morais <viniciusm@take.net>
2019-10-01 14:26:30 -0300

    Add Entity Framework

---
+++ Api.Entity/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api.Entity
+{
+	public partial class MyDbContext : DbContext
+	{
+		public MyDbContext()
+		{
+
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseMySQL("Server=localhost;User Id=root;Password=1234;Database=treinaweb");
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{ }
+	}
+}
